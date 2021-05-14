99f3ae7d4a4deaad91d364801e362bff4e420088
zenocoz <federico.soncini@gmail.com>
2021-05-14 15:09:40 +0200

    Updates

---
+++ precache-manifest.0db16c39bf7efc941f824f868c4f1f70.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1db148ddb22e1a190db9b3ad829d92a1",
+    "url": "/federicosoncini/index.html"
+  },
+  {
+    "revision": "d2e5367efbfb99a137a6",
+    "url": "/federicosoncini/static/css/2.d64471e2.chunk.css"
+  },
+  {
+    "revision": "5164f32e60979288c7b4",
+    "url": "/federicosoncini/static/css/main.cc1cfa94.chunk.css"
+  },
+  {
+    "revision": "d2e5367efbfb99a137a6",
+    "url": "/federicosoncini/static/js/2.6ab4cf48.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "/federicosoncini/static/js/2.6ab4cf48.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5164f32e60979288c7b4",
+    "url": "/federicosoncini/static/js/main.733a8522.chunk.js"
+  },
+  {
+    "revision": "249ccc6199907e200657",
+    "url": "/federicosoncini/static/js/runtime-main.0dbbbedc.js"
+  },
+  {
+    "revision": "c6bb37146dc5f75a8352640bec12fff6",
+    "url": "/federicosoncini/static/media/myProfile.c6bb3714.jpeg"
+  }
+]);
