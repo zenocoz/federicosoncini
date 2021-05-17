9fe6f61a57c1c46fec4e04fe5313ec9d821405ef
zenocoz <federico.soncini@gmail.com>
2021-05-17 10:20:40 +0200

    changes

--- src/App.js
+++ src/App.js
@@ -120,10 +120,10 @@ class App extends Component {
           resumeBasicInfo={this.state.resumeData.basic_info}
           sharedBasicInfo={this.state.sharedData.basic_info}
         />
-        {/* <Projects
-					resumeProjects={this.state.resumeData.projects}
-					resumeBasicInfo={this.state.resumeData.basic_info}
-				/> */}
+        <Projects
+          resumeProjects={this.state.resumeData.projects}
+          resumeBasicInfo={this.state.resumeData.basic_info}
+        />
         <Skills
           sharedSkills={this.state.sharedData.skills}
           resumeBasicInfo={this.state.resumeData.basic_info}
